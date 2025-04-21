5d926f1dc67188f6915f48b9bc2320d1f3499280
iamdavidpd <jd.perezd1@uniandes.edu.co>
2025-04-21 10:42:30 -0500

    cipher decipher

---
+++ src/Simetrico.java
@@ -0,0 +1,33 @@
+public class Simetrico {
+    private final static String PADDING = "AES/ECB/PKCS5Padding";
+
+    public static byte[] cifrar(SecretKey llave, String texto){
+        byte[] textoCifrado;
+
+        try {
+            Cipher cifrador = Cipher.getInstance(PADDING);
+            byte[] textoClaro = texto.getBytes();
+
+            cifrador.init(Cipher.ENCRYPT_MODE, llave);
+            textoCifrado = cifrador.doFinal(textoClaro);
+
+            return textoCifrado;
+        } catch (Exception e){
+            System.out.println("Excepcion: " + e.getMessage());
+            return null;
+        }
+    }
+
+    public static byte[] descifrar(SecretKey llave, byte[] texto){
+        byte[] textoClaro;
+
+        try {
+            Cipher	cifrador = Cipher.getInstance(PADDING);
+            cifrador.init(Cipher.DECRYPT_MODE, llave);
+            textoClaro = cifrador.doFinal(texto);
+        } catch (Exception e) {
+            System.out.println("Excepcion: " + e.getMessage());
+            return null;
+        }
+    }
+}
